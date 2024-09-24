18647c139a002066eabddcafa65bd4072c431748
Andrey Karazhev <karazhev@gmail.com>
2024-09-24 23:06:28 +0300

    Implemented the documentation.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # youtube-transcript
+
 YouTube Transcript Extractor
+
+Requirements:
+
+- pip install pytube3 youtube-transcript-api spacy
+- python -m spacy download en_core_web_lg
+- python -m spacy download ru_core_news_lg
--- youtube-transcript.py
+++ youtube-transcript.py
@@ -1,6 +1,3 @@
-# pip install pytube3 youtube-transcript-api spacy
-# python -m spacy download en_core_web_lg
-# python -m spacy download ru_core_news_lg
 from pytube import extract
 from heapq import nlargest
 from youtube_transcript_api import YouTubeTranscriptApi
